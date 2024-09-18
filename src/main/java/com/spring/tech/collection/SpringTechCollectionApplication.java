8a0d6e6c2d6f24ea90b9735345aefd7567afb243
jaeminlim92 <jaeminlim159@gmail.com>
2024-09-19 08:41:33 +0900

    inital project

---
+++ src/main/java/com/spring/tech/collection/SpringTechCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.tech.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTechCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTechCollectionApplication.class, args);
+    }
+
+}
